e9703d12e76e47e12356562181fe8f78cc879919
LiadMadmon <88856727+LiadMadmon@users.noreply.github.com>
2021-08-13 01:15:57 +0300

    Add Modal Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
 import "./styles.css";
-import {Text} from 'wix-style-react';
+import { CreateCouponPage } from "./Bookings/components/CreateCouponPage";
 
 export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+  return <CreateCouponPage />;
 }
